deb5273e94120b38e5bdae38a4eda2775893e0d5
Naveen Setty <naveenkumarpg@gmail.com>
2021-04-14 08:46:53 +0100

    Removed settings file.

--- odyssey_One/server/src/index.js
+++ odyssey_One/server/src/index.js
@@ -3,7 +3,7 @@ const typeDefs = require("./schema");
 
 const mocks = {
   Query: () => ({
-    tracksForHome: () => new MockList([6, 9]),
+    tracksForHome: () => new MockList([30, 40]),
   }),
   Track: () => ({
     id: () => "track_01",
